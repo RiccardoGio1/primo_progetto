d0dc01e81f2757112ef1e9b08b21414bb7d450f8
RiccardoGio1 <giovenzana.riccardo@gmail.com>
2025-10-27 13:45:18 +0100

    Passo9: add index generale

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from prima_app.views import homepage
-from prima_app.views import welcome
-from prima_app.views import lista
-from prima_app.views import chi_siamo
+from prima_app.views import homepage,welcome,lista,chi_siamo,variabili,index
 
+#RICORDO SEMPRE DI IMPORTARE DA PRIMA APP
 app_name="prima_app"
 urlpatterns=[
-    path('', homepage, name='homepage'),
+    path('homepage', homepage, name='homepage'),
     path('welcome', welcome, name='welcome'),
     path('lista',lista,name='lista'),
     path('chi_siamo',chi_siamo,name='chi_siamo'),
+    path('variabili',variabili,name='variabili'),
+    path('',index,name='index'),
 ]
