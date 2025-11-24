f55492dd6f8751cc4df9e3393d1b1fc75bed42e7
RiccardoGio1 <giovenzana.riccardo@gmail.com>
2025-11-24 13:15:20 +0100

    Verifica1

--- prova_pratica_0/views.py
+++ prova_pratica_0/views.py
@@ -29,4 +29,4 @@ def media(request):
         'list2': [1,2,3,4,5,6,7,8,9,10],
         'media':media
     }
-    return render(request,"prova_pratica_0/media.html ",context)
+    return render(request,"prova_pratica_0/media.html",context)
