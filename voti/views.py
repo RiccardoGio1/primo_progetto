ec248bcfedf56aef039550732d2ae86293e34476
RiccardoGio1 <giovenzana.riccardo@gmail.com>
2026-02-16 12:18:26 +0100

    Passo22_Finito

--- voti/views.py
+++ voti/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+voti = {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
+            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
+            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
 #1) lista delle materie
 def view_a(request):
     materie = ["Matematica","Italiano","Inglese","Storia","Geografia"]
@@ -9,17 +11,12 @@ def view_a(request):
 
 #2) visualizzare il contenuto del seguente dizionario dei voti : studente : [(materia,voto,assenze)]:
 def view_b(request):
-    voti = {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
-            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
-            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
     # Passa tutto il dizionario al template
     return render(request, 'voti/StudentiConVoto.html', {'voti': voti})
 
 #3) visualizzare la media dei voti di ciascuno studente
 def view_c(request):
-    voti = {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
-            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
-            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
+    
     medie = {}
     for studente,materie in voti.items():
         somma=0
@@ -32,10 +29,7 @@ def view_c(request):
 
 #4) visualizzare i voti massimo e minimo, le materie in cui si sono registrati e gli studenti che li hanno ottenuti
 def view_d(request):
-    voti = {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
-            'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
-            'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
-    
+        
     min=10
     max=0
     StudentiMax=[]
@@ -44,18 +38,30 @@ def view_d(request):
         for nomeM,voto,assenze in materie:
             if(voto>max):
                 max=voto
-                studente_max=studente
-                materie_max=nomeM
-                StudentiMax=[max,studente_max,materie_max]
+            
             if(voto<min):
                 min=voto
+                            
+    for studente,materie in voti.items():
+        for nomeM,voto,assenze in materie:
+            if(voto==max):
+                studente_max=studente
+                materie_max=nomeM
+                StudentiMax.append((studente_max,materie_max))
+                
+            if(voto==min):
                 studente_min=studente
                 materie_min=nomeM
-                StudentiMin=[min,studente_min,materie_min]
-                
-    
-    #return render(request, 'voti/media_studenti.html')     
+                StudentiMin.append((studente_min,materie_min))
+            
+        context = {
+        'max':max,
+        'min':min,
+        'studenti_max': StudentiMax,
+        'studenti_min': StudentiMin
+        }    
+    return render(request, 'voti/Max_Min.html',context)     
            
 
 def index5(request):
-    return render(request,"voti/Max_Min.html",)
+    return render(request,'voti/index5.html')
